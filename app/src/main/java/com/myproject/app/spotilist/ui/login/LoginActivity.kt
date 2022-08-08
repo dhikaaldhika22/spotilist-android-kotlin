144d09cc06024420cff093243997b2494e0d2b9d
Reyhan <mryhn101@gmail.com>
2022-08-08 21:27:06 +0700

    Add Drawable Icon of Spotlist Add 1 color "Background" Change the splash screen layout

---
+++ app/src/main/java/com/myproject/app/spotilist/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.myproject.app.spotilist.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myproject.app.spotilist.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/myproject/app/spotilist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/myproject/app/spotilist/ui/main/MainViewModel.kt
@@ -1,4 +1,5 @@
 package com.myproject.app.spotilist.ui.main
 
 class MainViewModel {
+
 }
