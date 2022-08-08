d8ae900ef0b5bfbd309eec30baa7763b8a5b59a1
dhikaaldhika22 <dhikaaldhika019@gmail.com>
2022-08-08 22:46:13 +0700

    Add Bottom Navigation and Home Fragment

--- app/src/main/java/com/myproject/app/spotilist/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/myproject/app/spotilist/ui/splashscreen/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
 package com.myproject.app.spotilist.ui.splashscreen
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.myproject.app.spotilist.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
