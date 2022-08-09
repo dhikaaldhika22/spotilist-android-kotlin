87b75b84eb394b5637d1b530c00c20ce494e45e7
dhikaaldhika22 <dhikaaldhika019@gmail.com>
2022-08-09 12:05:06 +0700

    Add SearchFragment and FavoriteFragment

--- app/src/main/java/com/myproject/app/spotilist/ui/MainActivity.kt
+++ app/src/main/java/com/myproject/app/spotilist/ui/MainActivity.kt
@@ -2,11 +2,44 @@ package com.myproject.app.spotilist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.myproject.app.spotilist.R
+import com.myproject.app.spotilist.databinding.ActivityMainBinding
+import com.myproject.app.spotilist.ui.favorite.FavoriteFragment
+import com.myproject.app.spotilist.ui.home.HomeFragment
+import com.myproject.app.spotilist.ui.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setItemSelected(R.id.nav_home, true)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.nav_host_fragment, HomeFragment())
+            .commit()
+
+        navBottom()
+    }
+
+    private fun navBottom() {
+        binding.navView.setOnItemSelectedListener {
+            var fragment: Fragment? = null
+            fragment = when (it) {
+                R.id.nav_search -> SearchFragment()
+                R.id.nav_favorite -> FavoriteFragment()
+                else -> HomeFragment()
+            }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit()
+        }
     }
 }
