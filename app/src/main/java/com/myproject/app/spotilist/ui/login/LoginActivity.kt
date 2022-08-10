b8039951098ff234c92c8e556695d38767a6fca9
Reyhan <mryhn101@gmail.com>
2022-08-10 15:37:33 +0700

    Update Splash Screen

--- app/src/main/java/com/myproject/app/spotilist/ui/login/LoginActivity.kt
+++ app/src/main/java/com/myproject/app/spotilist/ui/login/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.myproject.app.spotilist.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.myproject.app.spotilist.R
 
-class LoginActivity : AppCompatActivity() {
+abstract class LoginActivity : AppCompatActivity(), View.OnTouchListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
+
 }
--- app/src/main/java/com/myproject/app/spotilist/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/myproject/app/spotilist/ui/splashscreen/SplashScreenActivity.kt
@@ -1,14 +1,38 @@
 package com.myproject.app.spotilist.ui.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.myproject.app.spotilist.R
+import com.myproject.app.spotilist.databinding.ActivitySplashScreenBinding
+import com.myproject.app.spotilist.ui.MainActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+       initAction()
     }
+
+    private fun initAction() {
+        binding.apply {
+            ivLogo.alpha = 0f
+            ivLogo.animate().setDuration(2000).alpha(1f).withEndAction {
+                val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+    }
+
 }
+
