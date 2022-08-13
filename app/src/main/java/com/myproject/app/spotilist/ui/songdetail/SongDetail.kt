8ff2fdb823b183099372784f6c323a3b45e3f932
Reyhan <mryhn101@gmail.com>
2022-08-13 16:42:20 +0700

    Add Song Detail Activity and Played Song Activity

---
+++ app/src/main/java/com/myproject/app/spotilist/ui/songdetail/SongDetail.kt
@@ -0,0 +1,12 @@
+package com.myproject.app.spotilist.ui.songdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myproject.app.spotilist.R
+
+class SongDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_detail)
+    }
+}
